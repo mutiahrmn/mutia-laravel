37df8974ac9bbb593383901f180867b8630d16a6
Mutia Herman <mutiaherman282316@gmail.com>
2025-10-02 00:00:06 +0700

    Mengenal View Laravel

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Halo Laravel';
     return view('index');
 });
 
-Route::get('/about', function () {
-    return 'Ini adalah halaman <b>about</b>';
+Route::get('about', function () {
     return view('about');
 });
 
-Route::get('/mahasiswa', function () {
-    return 'Ini adalah halaman <b>mahasiswa </b>';
+Route::get('mahasiswa', function () {
     return view('mahasiswa');
 });
 
-Route::get('/profile', function () {
-    $nama = 'Mutia Herman';
-    return 'Ini adalah halaman <b>profile</b>, nama saya adalah ' . $nama;
+Route::get('profile', function () {
+    $nama = "Mutia Herman";
+    //return view('profile', compact('nama'));
     return view('profile')->with('nama', $nama);
 });
