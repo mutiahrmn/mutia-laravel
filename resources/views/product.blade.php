a533bd8c94e54225fc63bfb355625d60a94b16d0
Mutia Herman <mutiaherman282316@gmail.com>
2025-11-03 20:25:30 +0700

    Form Tambah Produk

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,8 +5,75 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Product</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" 
+     integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    
+     <nav class="navbar bg-body-tertiary fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Ujian Tengah Semester Laravel</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Ujian Tengah Semester Laravel</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="/product">Product</a>
+          </li>
+        </ul>
+        <form class="d-flex mt-3" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</nav>
+ <div class="container" style="margin-top: 80px;">
+        <div class="d-flex justify-content-between align-items-center">
+            <h2>Halaman Produk</h2>
+            <a href="/produk/tambah" class="btn btn-primary">
+                Tambah Produk
+            </a>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row">
+                <table class="table table-striped table-info">
+                    <thead>
+                        <tr>
+                            <th>Kode Produk</th>
+                            <th>Nama Produk</th>
+                            <th>Jenis Produk</th>
+                            <th>Harga Produk</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($products as $product)
+                        <tr>
+                            <td>{{ $product['kode_produk'] }}</td>
+                            <td>{{ $product['nama_produk'] }}</td>
+                            <td>{{ $product['jenis_produk'] }}</td>
+                            <td>{{ $product['harga_produk'] }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+
+                </table>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,29 @@ Route::get('/', function () {
 Route::get('product', function () {
     return view('product');
 });
+
+Route::get('/product', function () {
+    $page_title = 'Product';
+
+    $products = [
+        [
+            "kode_produk" => "BRG001",
+            "nama_produk" => "Pena",
+            "jenis_produk" => "Alat Tulis",
+            "harga_produk" => 20000
+        ],
+        [
+            "kode_produk" => "BRG002",
+            "nama_produk" => "Buku",
+            "jenis_produk" => "Alat Tulis",
+            "harga_produk" => 15000
+        ]
+    ];
+
+    return view('product', compact('page_title', 'products'));
+});
+
+
 // Route::get('about', function () {
 //     return view('about');
 // });
