98d8d390df630dab871a4f29a72ab99bdde90a11
Mutia Herman <mutiaherman282316@gmail.com>
2025-11-03 19:54:06 +0700

    Halaman Product

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,7 +25,7 @@
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Product</a>
+            <a class="nav-link" href="/product">Product</a>
           </li>
         </ul>
         <form class="d-flex mt-3" role="search">
--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,19 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('about', function () {
-    return view('about');
+Route::get('product', function () {
+    return view('product');
 });
+// Route::get('about', function () {
+//     return view('about');
+// });
 
-Route::get('mahasiswa', function () {
-    return view('mahasiswa');
-});
+// Route::get('mahasiswa', function () {
+//     return view('mahasiswa');
+// });
 
-Route::get('profile', function () {
-    $nama = "Mutia Herman";
-    //return view('profile', compact('nama'));
-    return view('profile')->with('nama', $nama);
-});
+// Route::get('profile', function () {
+//     $nama = "Mutia Herman";
+//     //return view('profile', compact('nama'));
+//     return view('profile')->with('nama', $nama);
+// });
